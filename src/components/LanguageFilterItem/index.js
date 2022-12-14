39b3bd79b109fa8be7ff969c5ab1fae8d990223a
prashanth reddy <prashanthreddymanchal9@gmail.com>
2022-12-14 09:42:17 +0000

    GitHub PopularRepos APP

---
+++ src/components/LanguageFilterItem/index.js
@@ -0,0 +1,30 @@
+// Write your code here
+import './index.css'
+
+const LanguageFilterItem = props => {
+  const {
+    isSelected,
+    languageFilter,
+    setSelectedLanguageFilterAndGetRepositories,
+  } = props
+  const btnClassName = isSelected
+    ? 'language-btn selected-language-btn'
+    : 'language-btn'
+  const onClickBtnLanguageFilter = () => {
+    setSelectedLanguageFilterAndGetRepositories(languageFilter.id)
+  }
+
+  return (
+    <li>
+      <button
+        className={btnClassName}
+        onClick={onClickBtnLanguageFilter}
+        type="button"
+      >
+        {languageFilter.language}
+      </button>
+    </li>
+  )
+}
+
+export default LanguageFilterItem
